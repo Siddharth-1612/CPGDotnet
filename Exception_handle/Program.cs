e426e9e8b22154f62d79bbadb08fab6c6f2f67a2
Siddharth-1612 <sidrao650@gmail.com>
2025-03-08 13:27:35 +0530

    commit

---
+++ Exception_handle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Accessing the 10th element: " + numbers[10]);
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Error: Attempted to access an index that is out of bounds.");
+            Console.WriteLine("Exception Message: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Execution completed.");
+        }
+    }
+}
